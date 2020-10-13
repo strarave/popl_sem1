08b671eec5f1b19ee8e9a7db9feb38c50b833cca
strarave <10609885@polimi.it>
2020-10-13 18:57:32 +0200

    adding another continuation example TODO: finish it, "is the sum of" function

--- macros_continuations.rkt
+++ macros_continuations.rkt
@@ -52,6 +52,30 @@
     (set! i (+ i 1))
     (mywhile)))
 
+
+(define *paths* '())
+(define fail #f)
+
+(define (choose l)
+  (if (null? l) (fail) 
+    (call/cc (lambda(cc) 
+      (set! *paths* (cons (lambda() 
+          (cc (choose (cdr l)))) 
+            *paths*)) 
+            (car l)))))
+
+;; the continuations for the fail branch
+
+(define (is-the-sum-of n)
+  (unless (and (>= n 0) (<= n 10))
+    (error "out of range " n))
+    (let ((x (choose '(0 1 2 3 4 5 6))
+          (y (choose '(0 1 2 3 4 5 6)))
+          (if (= (+ x y) n) (list x y) (fail))))))
+
+
+
+
 (define (break-negative l)
   (call/cc (lambda (break)
     (for-each (lambda(x)
