9c01d9caec4382d7e48cc451b0af902127aef858
strarave <10609885@polimi.it>
2020-10-26 13:13:35 +0100

    finish exercise session on haskell

--- haskell/es1.hs
+++ haskell/es1.hs
@@ -17,4 +17,12 @@ fibonacci n | n == 0 = 0
 rightTriangle :: Integer -> [(Integer, Integer, Integer)]
 rightTriangle n = [(a, b, c) | a <- [1..n], b <- [1..n], c <- [1..n], a^2 == b^2 + c^2]
 
+pack :: Eq a => [a] -> [[a]]
+pack l = packAux l [] []
 
+packAux :: Eq a => [a] -> [a] -> [[a]] -> [[a]]
+packAux [] currPack acc = currPack : acc
+packAux (x:xs) [] acc = packAux xs [x] acc
+packAux (x:xs) currPack@(y:ys) acc
+    | x == y = packAux xs (x:currPack) acc
+    | otherwise = packAux (x:xs) [] (currPack:acc)
