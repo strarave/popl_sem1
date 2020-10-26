744448cd0c1db810df47f441a7de759eb6ce7625
strarave <10609885@polimi.it>
2020-10-26 16:24:42 +0100

    added a stack structure to implement nested yielding. should correct the nesting procedure though

--- scheme/coroutines.rkt
+++ scheme/coroutines.rkt
@@ -1,22 +1,43 @@
 #lang racket
 
-;; the stored function
-(define stored #f)
+;; the function stack (FIFO manged list of lambdas)
+(define *stack* '())
+
+;; function to save a lambda on top of the stack
+;; must be called to initialize the block to yield to the execution
+(define (enqueue proc)
+    (set! *stack* (cons proc *stack*)))
+
+;; "pop" operation from the stack
+;; called by the yield thunk itself to move to the coroutine block
+(define (dequeue)
+    (let ((popped (car *stack*)))
+        (set! *stack* (cdr *stack*))
+        popped))
+
 
 ;; function to yield the execution to a saved block
 (define (yield)
+    (displayln "yielding...")
     (call/cc(
         lambda(cc)
-            (stored)
+            ((dequeue))
             (cc)
     )))
 
 ;; test function
 (define (yield-test)
-    (displayln "prima")
+    (displayln "before")
     (yield)
-    (displayln "dopo"))
+    (displayln "after"))
+
+;; some other test functions
+(define (first-level)
+    (displayln "first level!")
+    (yield))
+(define (second-level)(displayln "second level!"))
 
 ;; main
-(set! stored (lambda() (displayln "stored")))
+(enqueue second-level)
+(enqueue first-level)
 (yield-test)
