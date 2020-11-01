3253cf6e709fa08b7a09d3aacbe97f7e2fb04623
strarave <10609885@polimi.it>
2020-11-01 16:39:57 +0100

    implemented filter map reduce functions. experimenting with infinte computations

--- haskell/drafts.hs
+++ haskell/drafts.hs
@@ -1,5 +1,8 @@
 module DRAFTS where
 
+-- simple implementation of the basic filter, map, reduce functions
+-- and some other experiment with lazy evaluation on infinite computation
+
 numsFrom :: Integer -> [Integer]
 numsFrom n = n : (numsFrom $ n + 1)
 
@@ -7,3 +10,14 @@ numsFrom n = n : (numsFrom $ n + 1)
 inject :: (a -> a -> a) -> [a] -> a
 inject f (x:(xs:[])) = f x xs 
 inject f (x:(xs:xss)) = f (f x xs) (inject f xss)
+
+-- map implementation
+traspose :: (a -> b) -> [a] -> [b]
+traspose _ [] = []
+traspose f (h:t) = (f h) : (traspose f t)
+
+-- filter implementation
+sieve :: (a -> Bool) -> [a] -> [a]
+sieve _ [] = []
+sieve fil (h:t) | fil h = h : (sieve fil t)
+                | otherwise = sieve fil t
