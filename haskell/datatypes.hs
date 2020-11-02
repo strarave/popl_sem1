3a5833f88a71baf8dbdca3f843b75f5b034c1834
strarave <10609885@polimi.it>
2020-11-02 15:49:53 +0100

    experimenting with data structures

---
+++ haskell/datatypes.hs
@@ -0,0 +1,6 @@
+module DATATYPES where
+    data Tree a = Leaf a | Branch (Tree a) (Tree a)
+
+    fringe :: Tree a -> [a]
+    fringe (Leaf x) = [x]
+    fringe (Branch l r) = fringe l ++ fringe r 
