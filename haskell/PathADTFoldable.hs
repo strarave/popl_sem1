51e0fe66139b26a193c677ee60393da361851b13
Elia <10609885@polimi.it>
2020-11-17 16:09:43 +0100

    removed useless data constructor

--- haskell/PathADTFoldable.hs
+++ haskell/PathADTFoldable.hs
@@ -1,17 +1,14 @@
 -- another ADT that represents a vector path
 data Path a = 
   Empty |
-  End a a | 
   Next a a (Path a) 
   deriving (Show, Eq)
 
 -- getters
 abscissa :: (Path a) -> a
-abscissa (End a b) = a
 abscissa (Next a b _) = a
 
 ordinate :: (Path a) -> a
-ordinate (End a b) = b
 ordinate (Next a b _) = b
 
 -- property function
@@ -22,22 +19,21 @@ isEmpty _ = False
 -- stepper function (as "left, right" for the TREEADT)
 step :: (Path a) -> (Path a)
 step (Next _ _ n) = n
-step (End a b) = (End a b)
+step Empty = Empty
 
 -- converter to list
 toList :: (Path a) -> [[a]]
 toList Empty = []
-toList (End a b) = ((a:(b:[])):[])
 toList (Next a b n) = ((a:(b:[])):(toList n))
 
 -- FOLD
 instance Foldable Path where
-  foldr f z (End a b) = f b $ f a z
+  foldr f z Empty = z
   foldr f z (Next a b n) = f a $ f b $ foldr f z n
 
 -- MAP
 instance Functor Path where
-  fmap f (End a b) = End (f a) (f b)
+  fmap f Empty = Empty
   fmap f (Next a b n) = Next (f a) (f b) (fmap f n)
 
 -- path concatenation: it's just and append function for paths
@@ -45,7 +41,6 @@ instance Functor Path where
 pconcat :: (Path a) -> (Path a) -> (Path a)
 pconcat Empty p = p
 pconcat p Empty = p
-pconcat (End a b) n = (Next a b n)
 pconcat (Next a b n) z = (Next a b (pconcat n z))
 
 -- mono parameter concatenation
@@ -56,11 +51,10 @@ pconc p = pconcat Empty p
 -- and concatenate them
 pflatten :: Path (Path a) -> Path a
 pflatten Empty = Empty
-pflatten (End p1 p2) = pconcat p1 p2
 -- the pattern for "Next" should take account of all possible nesting
 pflatten (Next p1 p2 n) = pconcat p1 $ pconcat p2 $ pconc $ pflatten n
 
 -- APPLICATIVE
 instance Applicative Path where
-  pure a = End a a 
+  pure a = (Next a a Empty)
   fp <*> dp = pflatten $ fmap (\f -> fmap f dp) fp
