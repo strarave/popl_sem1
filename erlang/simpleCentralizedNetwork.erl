67bcffeb45af4ecdea3fc130ecc4fda1421a604d
Elia <10609885@polimi.it>
2020-12-07 11:14:33 +0100

    Finally implemented stupid client server interaction and echo processes

--- erlang/simpleCentralizedNetwork.erl
+++ erlang/simpleCentralizedNetwork.erl
@@ -1,16 +1,53 @@
 -module(simpleCentralizedNetwork).
 -compile(export_all).
 
-client(Server, Msg) -> 
-    case Msg of
-        "quit" -> Server ! "quitting";
-        true -> 
-            io:format(Msg),
-            if 
-                rand:uniform() > 0.7 -> 
-                    Server ! string:concat("Message from ", self()),
-                    client(Server, "");
-                true -> 
-                    client(Server, "")
-            end,
+%-------------------------
+% simple echo process
+%-------------------------
+
+go() -> 
+    Pid2 = spawn(?MODULE, loop, []),
+    Pid2 ! {self(), "hello"},
+    receive 
+        {Pid2, Msg} -> io:format("P1 ~w~n", [Msg])    
+    end,
+    Pid2 ! quit.
+
+loop() ->
+    receive 
+        {From, Msg} -> 
+            From ! {self(), Msg},
+            loop();
+        quit -> true
+    end.
+
+srv() -> 
+    io:format("[SERVER] Spawning client~n"),
+    Client = spawn(?MODULE, client, [loop]),
+    Client ! {self(), ""},
+
+    receive
+        {Client, spawn} ->
+            io:format("[SERVER] Received spawn request~n"), 
+            srv();
+        {Client, selfquit} ->
+            io:format("[SERVER] Received suicide note from client~n"), 
+            true
+    end,
+    srv().
+
+client(loop) -> 
+    receive
+        {Sender, _} -> 
+            timer:sleep(1000),
+            G = rand:uniform(),
+            if
+                G > 0.5 -> 
+                    io:format("[CLIENT] Spawn new client~n"),
+                    Sender ! {self(), spawn},
+                    client(loop);
+                true ->
+                    io:format("[CLIENT] Ending the client~n"),
+                    Sender ! {self(), selfquit}
+            end
     end.
